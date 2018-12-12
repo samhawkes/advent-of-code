ddfb704979ac469e87886da3092a198e044d973b
Sam Hawkes <samthawkes@gmail.com>
2018-12-12 15:17:37 +0000

    Added 2018 day 1 solution
    Added part 1 and 2 solutions for day 1.

--- AdventOfCode/Years/2018/Days/Day1.cs
+++ AdventOfCode/Years/2018/Days/Day1.cs
@@ -1,4 +1,7 @@
 ﻿using AdventOfCode.Days;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace AdventOfCode.Years._2018.Days
 {
@@ -6,7 +9,59 @@ namespace AdventOfCode.Years._2018.Days
     {
         public void Run(string path)
         {
-            var list = FileReader.ReadCharToCharList(path);
+            var list = FileReader.ReadLineToStringList(path);
+
+            var frequency = CalculateTotalFrequency(list);
+            var duplicateFrequency = FindDuplicate(list);
+
+            Console.WriteLine($"The total frequency at the end of the sequence starting from 0 is: {frequency}.");
+            Console.WriteLine($"The first frequency that is reached twice is: {duplicateFrequency}.");
+        }
+
+        private int CalculateTotalFrequency(List<string> input)
+        {
+            var total = 0;
+
+            foreach (var element in input)
+            {
+                total += int.Parse(element);
+            }
+
+            return total;
+        }
+
+        private int FindDuplicate(List<string> input)
+        {
+            List<int> usedFrequencies = new List<int>();
+            var currentFrequency = 0;
+            bool duplicateFound = false;
+            int loopCount = 0;
+
+            usedFrequencies.Add(currentFrequency);
+
+            while (!duplicateFound)
+            {
+                loopCount++;
+                List<string> tempList = new List<string>();
+                tempList.AddRange(input);
+                while (!duplicateFound && tempList.Any())
+                {
+                    currentFrequency += int.Parse(tempList[0]);
+                    tempList.RemoveAt(0);
+
+                    if (usedFrequencies.Contains(currentFrequency))
+                    {
+                        duplicateFound = true;
+                    }
+                    else
+                    {
+                        usedFrequencies.Add(currentFrequency);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Total loop count was: {loopCount}.");
+            return currentFrequency;
         }
     }
 }
