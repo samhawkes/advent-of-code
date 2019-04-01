b3830253d8bc238173bc09be599d90ec7e3eb211
samhawkes <40278032+samhawkes@users.noreply.github.com>
2019-04-01 09:20:03 +0100

    - Added 2017.1 solutions.

---
+++ AdventOfCode/Years/2017/Days/Day1.cs
@@ -0,0 +1,69 @@
+﻿using AdventOfCode.Days;
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Years._2017.Days
+{
+    public class Day1 : IPuzzleDay
+    {
+        public void Run(string path)
+        {
+            var number = FileReader.ReadInputToString(path);
+
+            var sumOfAdjacentDigits = CalculateSumOfAdjacentDigits(number);
+            var sumOfHalfWayRoundDigits = CalculateSumOfHalfWayRoundDigits(number);
+
+            Console.WriteLine($"The sum of the digits that match their adjacent is: {sumOfAdjacentDigits}.");
+            Console.WriteLine($"The sum of the digits that match those half way round the list is: {sumOfHalfWayRoundDigits}.");
+        }
+
+        private int CalculateSumOfAdjacentDigits(string input)
+        {
+            var list = input.Select(x => x - '0').ToArray();
+            var length = list.Count();
+
+            var sum = 0;
+
+            for (int j = 0; j < length; j++)
+            {
+                if (j < length - 1)
+                {
+                    if (list[j] == list[j + 1])
+                        sum += list[j];
+                }
+                else
+                {
+                    if (list[j] == list[0])
+                        sum += list[j];
+                }
+            }
+
+            return sum;
+        }
+
+        private int CalculateSumOfHalfWayRoundDigits(string input)
+        {
+            var list = input.Select(x => x - '0').ToArray();
+            var length = list.Count();
+
+            var sum = 0;
+
+            for (int j = 0; j < length; j++)
+            {
+                if (j < length / 2)
+                {
+                    if (list[j] == list[j + length / 2])
+                        sum += list[j];
+                }
+                else if (j >= length / 2)
+                {
+                    if (list[j] == list[j - length / 2])
+                        sum += list[j];
+                }
+            }
+
+            return sum;
+        }
+
+    }
+}
