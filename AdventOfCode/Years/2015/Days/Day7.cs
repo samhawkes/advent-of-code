c5631692244e136036aa316edcf60e483484a430
samhawkes <40278032+samhawkes@users.noreply.github.com>
2019-05-20 21:07:28 +0100

    - Commiting Day7 and 8 work in progress

--- AdventOfCode/Years/2015/Days/Day7.cs
+++ AdventOfCode/Years/2015/Days/Day7.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Years._2015.Days
@@ -13,14 +14,101 @@ namespace AdventOfCode.Years._2015.Days
             var list = FileReader.ReadLineToStringList(path);
 
             var formattedInstructions = this.FormatInstructions(list);
+            _instructions = formattedInstructions;
 
-            var answer = this.CalculateAnswerValue(formattedInstructions.First(x => x.AnswerValue.Name.Equals("a")));
+            //var expression = GetExpressions();
+
+            var listOfWires = this.GetListOfWires(formattedInstructions);
+
+            //var answer = this.CalculateAnswerValue(formattedInstructions, listOfWires);
             //var noLogicInstructions = formattedInstructions.Where(x => x.LogicOperator.Equals(string.Empty));
+
+            // x = 5;
+            // y = x;
+            // z = x + y;
+
+            ushort val = 5;
+            var x = Expression.Constant(val);
+            var y = x;
+            var z = Expression.Add(x, y);
+            var answer = Expression.Lambda<Func<ushort>>(z).Compile()();
 
             Console.WriteLine($"The signal ultimately provided to wire \"a\" is : {answer}.");
         }
 
-        internal List<Instruction> FormatInstructions(List<string> inputList)
+        private List<Instruction> _instructions = new List<Instruction>();
+        private Dictionary<string, Expression> _expressions = new Dictionary<string, Expression>();
+
+        private Expression GetExpression(Instruction instruction)
+        {
+            switch (instruction.LogicOperator)
+            {
+                case "AND":
+                {
+                    var left = GetOperand(instruction.LeftValue.Name);
+                    var right = GetOperand(instruction.RightValue.Name);
+                    return Expression.And(left, right);
+                }
+                case "NOT":
+                {
+                    var right = GetOperand(instruction.RightValue.Name);
+                    return Expression.Not(right);
+                }
+                case "OR":
+                {
+                    var left = GetOperand(instruction.LeftValue.Name);
+                    var right = GetOperand(instruction.RightValue.Name);
+                    return Expression.Or(left, right);
+                }
+                case "LSHIFT":
+                {
+                    var left = GetOperand(instruction.LeftValue.Name);
+                    var right = GetOperand(instruction.RightValue.Name);
+                    return Expression.LeftShift(left, right);
+                }
+                case "RSHIFT":
+                {
+                    var left = GetOperand(instruction.LeftValue.Name);
+                    var right = GetOperand(instruction.RightValue.Name);
+                    return Expression.RightShift(left, right);
+                }
+                case "":
+                {
+                    return GetOperand(instruction.RightValue.Name);
+                }
+                default:
+                    throw new Exception();
+            }
+
+            //if (instruction.Equals("AND"))
+            //    z = x & y;
+            //else if (instruction.Equals("NOT"))
+            //    z = ~x;
+            //else if (instruction.Equals("OR"))
+            //    z = x | y;
+            //else if (instruction.Equals("LSHIFT"))
+            //    z = x << y;
+            //else if (instruction.Equals("RSHIFT"))
+            //    z = x >> y;
+        }
+
+        private Expression GetOperand(string text)
+        {
+            if(ushort.TryParse(text, out var value))
+            {
+                return Expression.Constant(value);
+            }
+            else if (_expressions.ContainsKey(text))
+            {
+                return _expressions[text];
+            }
+            else
+            {
+                return Expression.Empty();
+            }
+        }
+
+        private List<Instruction> FormatInstructions(List<string> inputList)
         {
             List<Instruction> instructions = new List<Instruction>();
 
@@ -44,15 +132,38 @@ namespace AdventOfCode.Years._2015.Days
             return instructions;
         }
 
-        internal UInt16? CalculateAnswerValue(Instruction instruction)
+        private List<CircuitComponent> GetListOfWires(List<Instruction> listOfInstructions)
+        {
+            List<CircuitComponent> wires = new List<CircuitComponent>();
+
+            foreach (var item in listOfInstructions)
+            {
+                CircuitComponent wire = new CircuitComponent(item.AnswerValue.Name);
+
+                wires.Add(wire);
+            }
+
+            return wires.Distinct().ToList();
+        }
+
+        private UInt16? CalculateAnswerValue(List<Instruction> listOfInstructions, List<CircuitComponent> listOfWires)
         {
             //do some recursion in here until I evaluate enough to get the answer.
 
-            if (instruction.AnswerValue.Value != null)
-                return instruction.AnswerValue.Value;
+            var targetWire = listOfInstructions.First(x => x.AnswerValue.Name.Equals("a"));
+
+            if (targetWire.AnswerValue.Value != null)
+                return targetWire.AnswerValue.Value;
+
+            return CalculateAnswerValue(listOfInstructions, listOfWires);
+
+
+
 
             //need to work out what I'm doing with this block with regards to the answer.
             //also need to cast to UInt32, then cast the answer back to UInt16
+
+            //Need to handle instruction._value.Name being "", and ignoring it from calculating the answer
 
             //if (instruction.Equals("AND"))
             //    z = x & y;
@@ -68,7 +179,7 @@ namespace AdventOfCode.Years._2015.Days
             return 0;
         }
 
-        internal class Instruction
+        private class Instruction
         {
             internal Instruction(string fullInput, CircuitComponent left, string logicOp, CircuitComponent right, CircuitComponent answer)
             {
@@ -91,7 +202,7 @@ namespace AdventOfCode.Years._2015.Days
             internal CircuitComponent AnswerValue { get; set; }
         }
 
-        internal class CircuitComponent
+        private class CircuitComponent
         {
             internal CircuitComponent(string name)
             {
--- AdventOfCode/Years/2015/Days/Day8.cs
+++ AdventOfCode/Years/2015/Days/Day8.cs
@@ -15,11 +15,14 @@ namespace AdventOfCode.Years._2015.Days
 
             var charactersOfCode = CalculateCharactersOfCode(list);
             var charactersInMemory = CalculateCharactersInMemory(list);
+            var charactersInEncodedString = CalculateCharactersInEncodedString(list);
 
             Console.WriteLine($"The number of characters of code in the list of inputs is: {charactersOfCode}.");
             Console.WriteLine($"The number of characters in memory for the strings in the list is: {charactersInMemory}.");
+            Console.WriteLine($"The number of characters in the list of encoded strings is: {charactersInEncodedString}.");
 
             Console.WriteLine($"\nThe number of characters of code, minus the number of characters in memory is: {charactersOfCode - charactersInMemory}.");
+            Console.WriteLine($"\nThe number of encoded characters, minus the number of characters of code is: {charactersInEncodedString - charactersOfCode}.");
         }
 
         private int CalculateCharactersOfCode(List<byte[]> list)
@@ -30,6 +33,22 @@ namespace AdventOfCode.Years._2015.Days
         private int CalculateCharactersInMemory(List<byte[]> list)
         {
             return list.Sum(x => Regex.Unescape(Encoding.Default.GetString(x)).Length - 2); //-2 because we're ignoring the start and end quotes
+        }
+
+        private int CalculateCharactersInEncodedString(List<byte[]> list)
+        {
+            foreach (var item in list)
+            {
+                var h = Encoding.Default.GetString(item);
+                var a = Regex.Escape(Encoding.Default.GetString(item));
+                var b = a.ToCharArray();
+                var c = a.Length;
+
+                Console.WriteLine(a);
+            }
+            //this isn't quite getting the right answer. When escaping \", it's returning \\". The AoC is expecting \\\". I'm not sure if I'm wrong or the site is :(
+
+            return list.Sum(x => Regex.Escape(Encoding.Default.GetString(x)).Length + 4); //+4 because we're adding "\ to the start and \" to the end of each string
         }
     }
 }
