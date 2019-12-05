a010d52cb2c2a7ee9a76487a1336de147fde0013
Sam Hawkes <40278032+samhawkes@users.noreply.github.com>
2019-12-05 14:40:37 +0000

    - Added 2019 Day 1

---
+++ AdventOfCode/Years/2019/Days/Day1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AdventOfCode.Days;
+
+namespace AdventOfCode.Years._2019.Days
+{
+    public class Day1 : IPuzzleDay
+    {
+        public void Run(string path)
+        {
+            var totalFuel = 0;
+            var modifiedTotalFuel = 0;
+            
+            var list = FileReader.ReadLineToStringList(path);
+
+            foreach (var module in list)
+            {
+                totalFuel += CalculateFuelForModule(int.Parse(module));
+            }
+
+            foreach (var module in list)
+            {
+                var moduleFuelNeeded = CalculateFuelForModule(int.Parse(module));
+                modifiedTotalFuel += moduleFuelNeeded;
+                
+                while (CalculateFuelForModule(moduleFuelNeeded) > 0)
+                {
+                    var fuelNeededForFuel = CalculateFuelForModule(moduleFuelNeeded);
+                    modifiedTotalFuel += fuelNeededForFuel;
+
+                    moduleFuelNeeded = fuelNeededForFuel;
+                }
+            }
+
+            Console.WriteLine($"The total fuel needed for the modules is : {totalFuel}.");
+            Console.WriteLine($"The total fuel needed for the modules and their fuel is : {modifiedTotalFuel}.");
+        }
+
+        private int CalculateFuelForModule(int moduleWeight)
+        {
+            return (moduleWeight / 3) - 2; // Does an integer division, which takes the floor by default
+        }
+
+    }
+}
