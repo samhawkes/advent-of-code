5493ff613097471dbb1f5bac8fd81d8ef76fb7a8
Sam <samhawkes@users.noreply.github.com>
2019-12-05 23:21:41 +0000

    - 2019.2.1 solution

--- AdventOfCode/Years/2019/Days/Day2.cs
+++ AdventOfCode/Years/2019/Days/Day2.cs
@@ -1,4 +1,6 @@
-﻿using AdventOfCode.Days;
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCode.Days;
 
 namespace AdventOfCode.Years._2019.Days
 {
@@ -7,9 +9,37 @@ namespace AdventOfCode.Years._2019.Days
         public void Run(string path)
         {
             var input = FileReader.ReadInputToCommaSeparatedIntList(path);
-            
-            
+
+            var modifiedList = Compute(input);
+
+            Console.WriteLine($"The value at position 0 after computation is: {modifiedList[0]}.");
         }
-        
+
+        private List<int> Compute(List<int> startList)
+        {
+            startList[1] = 12;
+            startList[2] = 2;
+            const int add = 1;
+            const int multiply = 2;
+            const int terminate = 99;
+
+            for (var i = 0; i < startList.Count - 3; i += 4)
+            {
+                switch (startList[i])
+                {
+                    case terminate:
+                        return startList;
+
+                    case add:
+                        startList[startList[i + 3]] = startList[startList[i + 1]] + startList[startList[i + 2]];
+                        break;
+
+                    case multiply:
+                        startList[startList[i + 3]] = startList[startList[i + 1]] * startList[startList[i + 2]];
+                        break;
+                }
+            }
+            return startList;
+        }
     }
 }
